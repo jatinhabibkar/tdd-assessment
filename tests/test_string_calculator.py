8692c9fffd8e8cc342addcf37ca06c5d016aa868
jatinhabibkar <jatinkrishnahabibkar@gmail.com>
2025-03-31 22:07:15 +0530

    Added test case to handle custom delimiter with existing constrains
    Test case failing invalid literal for int() with base 10: '2\n4'

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -10,24 +10,28 @@ class MyTestCase(unittest.TestCase):
         self.calculator = StringCalculator()
 
     def test_empty_input_case(self):
-        self.assertEqual(self.calculator.add(None), 0, "Expected 0 for an None input")
+        self.assertEqual(0,self.calculator.add(None), "Expected 0 for an None input")
 
     def test_empty_string_returns_zero(self):
-        self.assertEqual(self.calculator.add(""), 0, "Expected 0 for an empty string input")
+        self.assertEqual(0,self.calculator.add(""), "Expected 0 for an empty string input")
 
     def test_one_number_case_return_sum(self):
-        self.assertEqual(self.calculator.add("1"), 1, "Expected 1 for an '1' string input")
+        self.assertEqual(1,self.calculator.add("1"), "Expected 1 for an '1' string input")
 
     def test_two_number_case_return_sum(self):
-        self.assertEqual(self.calculator.add("1,2"), 3, "Expected 3 for an '1,2' string input")
+        self.assertEqual(3,self.calculator.add("1,2"), "Expected 3 for an '1,2' string input")
 
     def test_n_number_case_return_sum(self):
-        self.assertEqual(self.calculator.add("8,7,10,13,1,1"), 40, "Expected 40 for an '8,7,10,13,1,1' string input")
+        self.assertEqual(40,self.calculator.add("8,7,10,13,1,1"), "Expected 40 for an '8,7,10,13,1,1' string input")
 
     def test_to_handle_new_line_instead_of_comma(self):
-        self.assertEqual(self.calculator.add("1\n2,3"),6,"Expected 6 for an '1\n2,3' string input")
+        self.assertEqual(6,self.calculator.add("1\n2,3"),"Expected 6 for an '1\n2,3' string input")
 
     def test_support_for_custom_delimiter(self):
-        self.assertEqual(self.calculator.add("//;\n1;2"),3,"Expected 3 for an '//;\n1;2' string input")
+        self.assertEqual(3,self.calculator.add("//;\n1;2"),"Expected 3 for an '//;\n1;2' string input")
+
+    def test_support_for_custom_delimiter_with_new_line_as_delimiter(self):
+        self.assertEqual( 7,self.calculator.add("//;\n1;2\n4"), "Expected 7 for an '//;\n1;2\n4' string input")
+
 if __name__ == '__main__':
     unittest.main()
